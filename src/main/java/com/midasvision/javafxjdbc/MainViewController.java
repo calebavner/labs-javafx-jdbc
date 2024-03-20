3ce3ac4f178f7c33004a7a892ad850c93a1c7f71
calebavner <avner.trabalho@hotmail.com>
2024-03-20 12:08:19 -0300

    Criação do MainViewController e bind com o SceneBuilder

--- src/main/java/com/midasvision/javafxjdbc/MainViewController.java
+++ src/main/java/com/midasvision/javafxjdbc/MainViewController.java
@@ -1,13 +1,38 @@
 package com.midasvision.javafxjdbc;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 
-public class MainViewController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainViewController implements Initializable {
     @FXML
-    private Label lb;
+    private MenuItem menuItemVendedor;
     @FXML
-    public void onClick() {
-        lb.setText("Funcionou");
+    private MenuItem menuItemDepartamento;
+    @FXML
+    private MenuItem menuItemSobre;
+
+    @FXML
+    public void onMenuItemVendedor() {
+        System.out.println("Menu Vendedor foi clicado");
+    }
+
+    @FXML
+    public void onMenuItemDepartamento() {
+        System.out.println("Menu Departamento foi clicado");
+    }
+
+    @FXML
+    public void onMenuItemSobre() {
+        System.out.println("Menu Sobre foi clicado");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
     }
 }
